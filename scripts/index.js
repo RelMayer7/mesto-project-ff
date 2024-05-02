287a09eebd3f4501595e629c77f75699ad1da2d0
Shepel Dmitry <RelMayer7@yandex.ru>
2024-05-02 14:47:24 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function CreateCard (link, name, DeleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+
+  deleteButton.addEventListener('click', DeleteCard);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function DeleteCard () {
+  const cardElement = document.querySelector('.card');
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item){
+  placesList.append(CreateCard(item.link, item.name, DeleteCard));
+});
